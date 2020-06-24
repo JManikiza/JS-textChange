c3f8ed22f4980482f7900a363b4dab069dbd78ee
Joven Manikiza <58902170+JManikiza@users.noreply.github.com>
2020-06-24 18:42:38 +0100

    added events, flavour text

--- history.js
+++ history.js
@@ -10,8 +10,8 @@ const wwii = document.getElementById('wwii');
 var history = document.getElementById('history-text');
 
 
-paleo.onclick = function() {
-    history.innerText = `The Paleolithic Age refers to a time period in history, from approximately 2.6
+paleo.onclick = function () {
+    document.getElementById('history-text').innerText = `The Paleolithic Age refers to a time period in history, from approximately 2.6
     million years ago to roughly 10,000 years ago. The Paleolithic Age covers the period from when stone
     tools were first used by man to the beginning of the Mesolithic Age. The Paleolithic Age is divided into
     three divisions, including the Lower, Middle, and Upper Paleolithic periods. During the Paleolithic Age
@@ -20,6 +20,103 @@ paleo.onclick = function() {
     develop early religions, and create art as can be seen in cave paintings dating back to this time.`;
 };
 
-egypt.onclick = function() {
-    history.innerText = `Bam!`
+egypt.onclick = function () {
+    document.getElementById('history-text').innerText = `By 5,000 BC the people of Egypt had begun farming. They also wove linen and made pottery. Later they learned to use bronze. About 3,200 BC the Egyptians invented writing. The first Egyptian in history was King Menes aka Narmer who lived shortly before 3,100 BC. At that time Egypt was divided into northern (lower) Egypt and southern (upper Egypt). About 3118 Menes managed to unite the two. He made Memphis his capital. Ancient Egypt was a highly organized society. The country was divided into 42 areas called nomes. Each one was governed by nomarch. Farmers paid part of their crops in taxes.
+
+    The first period of Egyptian history, which ended in 2181 BC is called the Old Kingdom. During it the pharaohs built pyramids. The first pyramid, the step pyramid was built by Zoser about 2665 BC. Others were built by the following pharaohs Sneferu and Khufu.
+    
+    However central authority in Egypt eventually weakened. After about 1281 BC Egypt split into parts and there were civil wars between the rival areas. This period of civil disorder is called the First Intermediate Period and it lasted until 2055 BC. Finally, Mentuhotep II succeeded in reuniting Egypt and he founded the Middle Kingdom.
+    
+    The Middle Kingdom lasted until 1650 BC. It was a great period of art and literature in Egypt. Furthermore, the pharaohs carried out successful military campaigns and more pyramids were built. However, the Middle Kingdom was followed by the Second Intermediate Period. About 1650 Palestinian people called the Hyksos seized power in northern Egypt. They ruled from the city of Avaris. However native Egyptians continued to rule southern Egypt and in 1550 BC they drove out the Hyksos and reunited Egypt. So began the New Kingdom. It lasted from 1550 to 1070 BC. During this era, Egypt was rich and powerful once again. Egypt controlled Nubia, the land to the south and invaded Palestine and Syria. Meanwhile, great new temples were built at Thebes and pharaohs were buried in the Valley of the Kings. Then about 1364 BC, Akhenaten became pharaoh. He worshiped only the sun god Aten. However, his son Tutankhamen worshiped the old gods.
+    
+    The New Kingdom collapsed in 1070 and gave way to another period of disunity. From this point, Egypt declined and never recovered its former glory.
+    
+    During the Third intermediate period Egypt split into two halves, north and south. However, in 747 BC kings from Nubia (the country south of Egypt) conquered Egypt and restored unity. Yet in 525 BC the Persians conquered Egypt. Then in 332, Alexander the Great conquered it.
+    
+    After Alexander's death his empire split into parts. A Greek general called Ptolemy eventually took Egypt and for about 300 years his Greek descendants ruled Egypt. However, in 30 BC Egypt became a province of the Roman Empire and ceased to be an independent kingdom.
+    
+    Meanwhile woman called Sobekneferu ruled Egypt around 1800 BC. Later a woman named Hatshepsut ruled Egypt c. 1479-1458 BC. Another woman, Twosret ruled Egypt c. 1191-1189 BC.
+    
+    However the real end of Ancient Egypt came with Christianity. It reached Alexandria by 70 AD and spread south by 180 AD. Christianity meant the end of the old religion and so the end of Ancient Egyptian culture.`
+};
+
+
+china.onclick = function () {
+    document.getElementById('history-text').innerText = `The early Chinese Civilization included the Xia Dynasty which remained from around 2100 BC until 1600 BC, and is described in the ancient Records of the Grand Historians, from which pottery and shells have been excavated that belonged to this period. After that, it was followed by the Shang Dynasty which existed from 1600-1046 BC. The oracle bones were found during this time which had divination records inscribed on it. The beginning of the Zhou Dynasty was witnessed in 1046 to 256 BC which actually emerged in the Yellow River Valley and they began their rule under the semi-feudal system. The spring and autumn or the Warring States Period saw the annexation by the local military leaders, and the modern system of Sheng and Xian emerged in the country.
+
+    Dynastic China
+    The foremost early dynasty was the Qin Dynasty, which came into being from 221-206 BC. In this, the Kings subdued the parts of the Han Chinese homeland and united them under a central legalized government. The other dynasty was Han Dynasty and its period consisted of 202 BC – AD 220. This was regarded as the golden age in the Chinese History, which actually consolidated the foundation of China. Xin dynasty period began in AD 9 and it was said to be the short-lived dynasty, but many economic reforms were undertaken during this period. The other main dynasties which followed after this was Jin Dynasty, Northern and southern dynasties, Sui Dynasty, Tang dynasty, Five dynasties and the Ten Kingdoms, Yuan Dynasties, and finally it reached the Qing Dynasty.
+    
+    Xinhai Revolution
+    The Xinhai Revolution is also known as the Chinese Revolution or the Revolution of 1911, which threw the last Imperial dynasty of China and that, was the Qing Dynasty. It led to the Formation of Republic of China. The revolution witnessed many revolts, and the uprisings and the main turning point of this movement was the Wuchang Uprising which was actually a result of mishandling of the Railway Protection Movement. It is seen that the Republic of China in Taiwan (or Formosa) and the People’s Republic of China each consider themselves as the true successors of the Xinhai Revolution.
+    
+`
+};
+
+romans.onclick = function () {
+    document.getElementById('history-text').innerText = `Founding (c. 625 BC)
+    Rome was founded around 625 BC in the areas of ancient Italy known as Etruria and Latium. It is thought that the city-state of Rome was initially formed by Latium villagers joining together with settlers from the surrounding hills in response to an Etruscan invasion. It is unclear whether they came together in defense or as a result of being brought under Etruscan rule. Archaeological evidence indicates that a great deal of change and unification took place around 600 BC which likely led to the establishment of Rome as a true city.
+    
+    Period of Kings (625-510 BC)
+    The first period in Roman history is known as the Period of Kings, and it lasted from Rome’s founding until 510 BC. During this brief time Rome, led by no fewer than six kings, advanced both militaristically and economically with increases in physical boundaries, military might, and production and trade of goods including oil lamps. Politically, this period saw the early formation of the Roman constitution. The end of the Period of Kings came with the decline of Etruscan power, thus ushering in Rome’s Republican Period.
+    
+    Republican Rome (510-31 BC)
+    Rome entered its Republican Period in 510 BC. No longer ruled by kings, the Romans established a new form of government whereby the upper classes ruled, namely the senators and the equestrians, or knights. However, a dictator could be nominated in times of crisis. In 451 BC, the Romans established the “Twelve Tables,” a standardized code of laws meant for public, private, and political matters.
+    
+    Rome continued to expand through the Republican Period and gained control over the entire Italian peninsula by 338 BC. It was the Punic Wars from 264-146 BC, along with some conflicts with Greece, that allowed Rome to take control of Carthage and Corinth and thus become the dominant maritime power in the Mediterranean.
+    
+    Soon after, Rome’s political atmosphere pushed the Republic into a period of chaos and civil war. This led to the election of a dictator, L. Cornelius Sulla, who served from 82-80 BC. Following Sulla’s resignation in 79 BC, the Republic returned to a state of unrest. While Rome continued to be governed as a Republic for another 50 years, the shift to Imperialism began to materialize in 60 BC when Julius Caesar rose to power.
+    
+    By 51 BC, Julius Caesar had conquered Celtic Gaul and, for the first time, Rome’s borders had spread beyond the Mediterranean region. Although the Senate was still Rome’s governing body, its power was weakening. Julius Caesar was assassinated in 44 BC and replaced by his heir, Gaius Julius Caesar Octavianus (Octavian) who ruled alongside Mark Antony. In 31 BC Rome overtook Egypt which resulted in the death of Mark Antony and left Octavian as the unchallenged ruler of Rome. Octavian assumed the title of Augustus and thus became the first emperor of Rome.
+    
+    Imperial Rome (31 BC – AD 476)
+    Rome’s Imperial Period was its last, beginning with the rise of Rome’s first emperor in 31 BC and lasting until the fall of Rome in AD 476. During this period, Rome saw several decades of peace, prosperity, and expansion. By AD 117, the Roman Empire had reached its maximum extant, spanning three continents including Asia Minor, northern Africa, and most of Europe.
+    
+    In AD 286 the Roman Empire was split into eastern and western empires, each ruled by its own emperor. The western empire suffered several Gothic invasions and, in AD 455, was sacked by Vandals. Rome continued to decline after that until AD 476 when the western Roman Empire came to an end. The eastern Roman Empire, more commonly known as the Byzantine Empire, survived until the 15th century AD. It fell when Turks took control of its capital city, Constantinople (modern day Istanbul in Turkey) in AD 1453.`
+};
+
+plague.onclick = function () {
+    document.getElementById('history-text').innerText = `1347 was the beginning of dark times for the City of London. Throughout the streets, rumours mingled and grew about a plague from the continent. By 1348, people began to display the symptoms: swollen lymph nodes, blackened fingers and toes, fever, and vomiting blood. Over 18 months, it spread throughout the city, killing 40,000 people, or nearly half of London. The conditions in the city were perfect for its spread, as sewage flowed in the streets and houses were small and often overcrowded.
+
+    Going by many names including “The Plague”, “The Black Death”, and “The Bubonic Plague”, the disease is caused by Yserinia pestis, a type of bacteria most commonly carried by fleas. The plague is believed to have begun in China where it killed approximately 25 million people before migrating west to Constantinople. From there, Italian merchants brought it back to continental Europe, and it continued to spread as Europeans did not have enough medical knowledge to stop it.
+    
+    40,000 deaths had a great impact on the city. Whereas the tradition then, as now, was for burying the dead in individual graves, mass graves (or “plague pits”) became the new norm. One of these was found last year during the construction of London’s Crossrail project. The plague virtually shut down the city as many shut themselves in their homes and trade halted. There are even accounts of grass beginning to grow in the streets as they went unused. It also halted the campaigns in the Hundred Years War.
+    
+    Eventually, deaths from the plague began to subside around 1350, but they caused irreversible social effects. The high mortality rate amongst the working classes led to a rise in wages and the eventual abolition of serfdom. Unfortunately, while the worst seemingly had passed, the disease would flare up every twenty to thirty years, killing 20% of the city’s population each time. The city instituted the first regulations to deal with the plague in 1518. Infected houses had to hang a bale of hay on a pole outside for 40 days, while the people who lived there had to carry a white stick when they went out. Additional rules included painting a cross on the doors of the plague-infested houses and burying people at night. Another plague struck in 1563 that killed almost 24% of the city’s population.
+    
+    Perhaps the worst of it began in 1665, dubbed “The Great Plague”. A growth in the city’s population meant that even more people were killed, this time around 100,000 Londoners (or 20% of the population). Anyone with money fled the city for the country in the hopes to avoid becoming infected themselves. Despite being referred to as “The Great Plague”, the fewer people died during this plague in the whole of Britain than during previous outbreaks. The use of the word “great” derives more from it being the final major instance of plague. The city began to keep weekly records of those who had died, known as “Bills of Mortality”. The greatest of these mortality bills in September 1665 recorded the names of 7,165 people.
+    
+    After that September, deaths slowly began to fall and those who had fled the city began to return in December. By February, King Charles II and his entourage returned to London. In fact, the deaths of so many in London made it a destination location as a place to start over and seek one’s fortune. Of course, the city was not safe for too long, but this time it was from a different threat as the Great Fire of London struck the city in September 1666. Between these two events, London experienced both a great rebuilding and influx of people from the country to the city. London grew in a way that would forever change it, becoming a city of life instead of a city of death.`
+};
+
+ottoman.onclick = function () {
+    document.getElementById('history-text').innerText = `The Ottoman Empire “The Exalted Ottoman State” was a state and caliphate that controlled much of Southeast Europe, Western Asia and North Africa between the 14th and early 20th centuries. It was founded at the end of the 13th centuries in northwestern Anatolia in the town of Sogut by the Oghuz Turkish tribal Leader Osman Ghazi! Initially, the dynasty was of Turkic Origin.
+
+    After 1354, the Ottomans crossed into Europe and with the conquest of the Balkans, the Ottoman beylik was transformed into transcontinental empire. During the 16th and 17th centuries, at the height of its power under the reign of Suleiman the Magnificient, the Ottoman Empire was a multinational, multilingual empire controlling most of Southeast Europe. Thus, over the course of the 19th century, the Ottoman state became vastly more powerful and organised, despite suffering further territorial losses, especially in the Balkans, where a number of new states emerged.
+    
+     
+    In early 20th century the empire allied with Germany, hoping to escape from the diplomatic isolation which had contributed to its recent territorial losses, and thus joined World War I on the side of the Central Powers. While the Empire was able to largely holds its own during the conflict, it was struggling with internal dissent, especially with the Arab Revolt in its Arabians holdings. During this time, genocide was committed by the Ottoman government against the Armenians.
+    
+    As the Seljuk Sultanate of Rum declined in the 13th century, Anatolia was divided into a patchwork of independent Turkish principalities known as the Anatolian Beyliks. One of these beyliks, in the region of Bithynia on the frontier of the Byzantine Empire, was led by the Turkish tribal leader Osman I, a figure of obscure origins from whom the name Ottoman is derived. Osman’s early followers consisted both of Turkish tribal groups and Byzantine renegades, many but not all converts to Islam.
+    
+     
+    Osman extended the control of his principality by conquering Byzantine towns along the Sakarya River. A Byzantine defeat at the battle of Bapheus in 1302 contributed to Osman’s rise as well. It is not well understood how the early Ottomans came to dominate their neighbours, due to the lack of sources surviving from this period. The Gaza Thesis theory popular during the twentieth century credited their success to their rallying of religious warriors to fight for them in the name of Islam, but it is now highly criticised and no longer generally accepted by historians, and no consensus on the nature of the early Ottoman state’s expansion has replaced it.
+    
+    By the end of Suleiman’s reign, the Empire spanned approximately 877,888 sq. mi, extending over three continents. The Ottoman Empire was a major part of the European political sphere. The Ottomans became involved in multi-continental religious wars when Spain and Portugal were under the Iberian Union, the Ottomans as holders of the Caliph title, meaning leader of all Muslims worldwide, and Iberians, as leaders of the Christian crusaders, were locked in a worldwide conflict.
+    
+     
+    This treaty of Ottoman Empire was able to enjoy a generation of peace, as Austria and Russia were forced to deal with the rise of Prussia. The history of the Ottoman Empire during the World War I began with the Ottoman surprise attack on the Russian Black Sea coast on 29 October 1914. Before the reforms of the 19th and 20th centuries, the state organisation of the Ottoman Empire was a system with two main dimensions.
+    
+     
+    The Sultan was the highest position in the system. The civil system was based on local administrative units based on the region’s characteristics. In the Ottoman Empire each millet established a schooling system serving its members. Education therefore was largely divided on ethnic and religious lines: few non-Muslims attended schools for Muslim students, and vice versa. Most institutions that did serve all ethnic and religious groups taught in French or in other languages.`
+};
+
+wwi.onclick = function () {
+    document.getElementById('history-text').innerText = `World War I (WW1) also known as the First World War, was a global war centered in Europe that began on 28th July 1914 and lasted until 11th November 1918. The war lasted exactly four years, three months and 14 days. Before World War II began in 1939, World War I was called the Great War, the World War or the War to End all Wars. 135 countries took part in World War I, and more than 15 million people died. See the fact file below for more information about World War I.
+
+    World War 1 was a military conflict lasting from 1914 to 1918 which involved nearly all the biggest powers of the world. It involved two opposing alliances – the Allies and the Central Powers. The countries of the Allies included Russia, France, British Empire, Italy, United States, Japan, Rumania, Serbia, Belgium, Greece, Portugal and Montenegro. The countries of the Central Powers included Germany, Austria-Hungary, Turkey and Bulgaria`
+};
+
+wwii.onclick = function () {
+    document.getElementById('history-text').innerText = `World War II (WWII), also known as the Second World War, was a global war which took place between 1939 to 1945. Most of the world’s countries, including all of the great powers, fought as part of two military alliances: the Allies and the Axis.`
 };
